3606456efc745dc4d62baf7e18244bad1e5f88f9
Nan Liu <nan.liu@gmail.com>
2014-09-10 12:57:09 -0700

    Manage archive file ownership and mode.

--- manifests/artifactory.pp
+++ manifests/artifactory.pp
@@ -3,7 +3,11 @@ define archive::artifactory (
   $server,
   $port,
   $url_path,
-  $archive_path,
+  $path         = $name,
+  $owner        = undef,
+  $group        = undef,
+  $mode         = undef,
+  $archive_path = undef,
   $ensure       = present,
   $extract      = undef,
   $extract_path = undef,
@@ -11,13 +15,23 @@ define archive::artifactory (
   $cleanup      = undef,
 ) {
 
+  include archive::params
+
+  if $archive_path {
+    $file_path = "${archive_path}/${name}"
+  } else {
+    $file_path = $path
+  }
+
+  validate_absolute_path($file_path)
+
   $art_url = "http://${server}:${port}/artifactory"
   $file_url = "${art_url}/${url_path}"
   $sha1_url = "${art_url}/api/storage/${url_path}"
 
   archive { $name:
     ensure        => $ensure,
-    path          => $archive_path,
+    path          => $file_path,
     extract       => $extract,
     extract_path  => $extract_path,
     source        => $file_url,
@@ -25,5 +39,12 @@ define archive::artifactory (
     checksum_type => 'sha1',
     creates       => $creates,
     cleanup       => $cleanup,
+  }
+
+  file { $file_path:
+    owner   => pick($owner, $archive::params::owner),
+    group   => pick($group, $archive::params::group),
+    mode    => pick($mode, $archive::params::mode),
+    require => Archive[$name],
   }
 }
