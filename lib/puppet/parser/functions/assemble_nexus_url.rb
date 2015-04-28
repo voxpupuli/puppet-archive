279f270eb1bbeffc1cf562fcb124edc8058d239d
Jairo Junior <junior.jairo1@gmail.com>
2015-04-27 23:33:53 -0300

    rubocop fix

--- lib/puppet/parser/functions/assemble_nexus_url.rb
+++ lib/puppet/parser/functions/assemble_nexus_url.rb
@@ -1,7 +1,7 @@
 module Puppet::Parser::Functions
   SERVICE_RELATIVE_URL = 'service/local/artifact/maven/content'
 
-  newfunction(:assemble_nexus_url, type: :rvalue) do |args|
+  newfunction(:assemble_nexus_url, :type => :rvalue) do |args|
     nexus_url = args[0]
     params = args[1]
     query_string = params.to_a.map { |x| "#{x[0]}=#{x[1]}" }.join('&')
