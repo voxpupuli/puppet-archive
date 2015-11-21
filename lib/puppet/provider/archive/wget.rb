378a0ebbd3db547d646f008c85d484275addf473
Joseph Yaworski <joseph.yaworski@fireeye.com>
2015-11-21 01:15:54 -0500

    Make the directory before moving the file there

--- lib/puppet/provider/archive/wget.rb
+++ lib/puppet/provider/archive/wget.rb
@@ -27,6 +27,7 @@ Puppet::Type.type(:archive).provide(:wget, :parent => :ruby) do
       fail(Puppet::Error, 'Download file checksum mismatch') unless archive.checksum(resource[:checksum_type]) == checksum
     end
 
+    FileUtils.mkdir_p(File.dirname(archive_filepath))
     FileUtils.mv(temppath, archive_filepath)
   end
 
