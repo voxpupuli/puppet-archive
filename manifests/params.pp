13cad5143c1d7feb1604c91cbf49fe3c07afda94
Joseph Herlant <herlantj@gmail.com>
2016-02-16 18:47:55 -0800

    Fixing tests failures on non-defined facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,6 @@
 # archive parameters
 class archive::params {
   case $::osfamily {
-    default: {
-      $path  = '/opt/staging'
-      $owner = '0'
-      $group = '0'
-      $mode  = '0640'
-      $seven_zip_name = undef
-      $seven_zip_provider = undef
-    }
     'Windows': {
       $path               = $::archive_windir
       $owner              = 'S-1-5-32-544' # Adminstrators
@@ -16,14 +8,24 @@ class archive::params {
       $mode               = '0640'
       $seven_zip_name     = '7zip'
       $seven_zip_provider = 'chocolatey'
+      if versioncmp($::puppetversion, '4.3.1') >= 0 {
+        $gem_provider = 'pe_gem'
+      } else {
+        $gem_provider = 'gem'
+      }
     }
-  }
-
-  if $::puppetversion =~ /Puppet Enterprise/ and $::osfamily != 'Windows' {
-    $gem_provider = 'pe_gem'
-  } elsif $::aio_agent_version {
-    $gem_provider = 'puppet_gem'
-  } else {
-    $gem_provider = 'gem'
+    default: {
+      $path  = '/opt/staging'
+      $owner = '0'
+      $group = '0'
+      $mode  = '0640'
+      $seven_zip_name = undef
+      $seven_zip_provider = undef
+      if $::puppetversion =~ /Puppet Enterprise/ or versioncmp($::puppetversion, '4.0.0') >= 0 {
+        $gem_provider = 'pe_gem'
+      } else {
+        $gem_provider = 'gem'
+      }
+    }
   }
 }
--- spec/classes/archive_spec.rb
+++ spec/classes/archive_spec.rb
@@ -17,8 +17,16 @@ describe 'archive' do
     it { is_expected.to_not contain_package('7zip') }
   end
 
+  context 'RHEL Puppet 4.0.0 opensource' do
+    let(:facts) { { :osfamily => 'RedHat', :puppetversion => '4.0.0' } }
+
+    it { is_expected.to contain_package('faraday').with_provider('pe_gem') }
+    it { is_expected.to contain_package('faraday_middleware').with_provider('pe_gem') }
+    it { is_expected.to_not contain_package('7zip') }
+  end
+
   context 'Windows Puppet opensource' do
-    let(:facts) { { :osfamily => 'Windows', :puppetversion => '3.7.3' } }
+    let(:facts) { { :osfamily => 'Windows', :archive_windir => 'C:/staging', :puppetversion => '3.7.3' } }
 
     it { is_expected.to contain_package('faraday').with_provider('gem') }
     it { is_expected.to contain_package('faraday_middleware').with_provider('gem') }
@@ -28,8 +36,19 @@ describe 'archive' do
     end
   end
 
+  context 'Windows Puppet 4.3.1 opensource' do
+    let(:facts) { { :osfamily => 'Windows', :archive_windir => 'C:/staging', :puppetversion => '4.3.1' } }
+
+    it { is_expected.to contain_package('faraday').with_provider('pe_gem') }
+    it { is_expected.to contain_package('faraday_middleware').with_provider('pe_gem') }
+    it do
+      should contain_package('7zip').with(:name     => '7zip',
+                                          :provider => 'chocolatey',)
+    end
+  end
+
   context 'Windows Puppet Enterprise' do
-    let(:facts) { { :osfamily => 'Windows', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
+    let(:facts) { { :osfamily => 'Windows', :archive_windir => 'C:/staging', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
 
     it { is_expected.to contain_package('faraday').with_provider('gem') }
     it { is_expected.to contain_package('faraday_middleware').with_provider('gem') }
@@ -40,7 +59,7 @@ describe 'archive' do
   end
 
   context 'with 7zip package' do
-    let(:facts) { { :osfamily => 'Windows', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
+    let(:facts) { { :osfamily => 'Windows', :archive_windir => 'C:/staging', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
 
     let(:params) do
       {
@@ -60,7 +79,7 @@ describe 'archive' do
   end
 
   context 'without 7zip' do
-    let(:facts) { { :osfamily => 'Windows', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
+    let(:facts) { { :osfamily => 'Windows', :archive_windir => 'C:/staging', :puppetversion => '3.4.3 (Puppet Enterprise 3.2.3)' } }
 
     let(:params) do
       {
