0f349a3090c3c816a148ab18f5b2dcdced929105
Roberto Andradas Izquierdo <randradas@gmail.com>
2016-03-16 21:29:36 +0100

    voxpopuli/puppet-archive#142 Fix Cookie header in wget arguments formatting.

--- lib/puppet/provider/archive/wget.rb
+++ lib/puppet/provider/archive/wget.rb
@@ -11,7 +11,7 @@ Puppet::Type.type(:archive).provide(:wget, :parent => :ruby) do
 
     params += optional_switch(resource[:username], ['--user=%s'])
     params += optional_switch(resource[:password], ['--password=%s'])
-    params += optional_switch(resource[:cookie], ['--header="Cookie: "%s"'])
+    params += optional_switch(resource[:cookie], ['--header="Cookie: %s"'])
     params += optional_switch(resource[:proxy_server], ["--#{resource[:proxy_type]}_proxy=#{resource[:proxy_server]}"])
 
     wget(params)
