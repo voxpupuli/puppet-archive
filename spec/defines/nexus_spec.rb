acdf27841fecccd2bf777a9976bbcf9e233bb7a4
Rick van den Hof <rick@vandenhof.eu>
2017-06-01 13:06:56 +0200

    Support Nexus 3 urls for artifact downloads
    Nexus 3 no longer supports the old url format.

--- spec/defines/nexus_spec.rb
+++ spec/defines/nexus_spec.rb
@@ -152,4 +152,34 @@ describe 'archive::nexus' do
 
     it { is_expected.to compile.and_raise_error(%r{parameter 'allow_insecure' expects a value of type Undef or Boolean, got String}) }
   end
+  context 'nexus archive with use_nexus3_urls => false' do
+    let(:title) { '/tmp/artifact.war' }
+
+    let(:params) do
+      {
+        url: 'https://oss.sonatype.org',
+        gav: 'io.hawt:hawtio-web:1.4.36',
+        repository: 'releases',
+        packaging: 'war',
+        use_nexus3_urls: false
+      }
+    end
+
+    it { is_expected.to contain_archive('/tmp/artifact.war').with_source('https://oss.sonatype.org/service/local/artifact/maven/content?g=io.hawt&a=hawtio-web&v=1.4.36&r=releases&p=war') }
+  end
+  context 'nexus archive with use_nexus3_urls => true' do
+    let(:title) { '/tmp/artifact.war' }
+
+    let(:params) do
+      {
+        url: 'https://oss.sonatype.org',
+        gav: 'io.hawt:hawtio-web:1.4.36',
+        repository: 'releases',
+        packaging: 'war',
+        use_nexus3_urls: true
+      }
+    end
+
+    it { is_expected.to contain_archive('/tmp/artifact.war').with_source('https://oss.sonatype.org/repository/releases/io/hawt/hawtio-web/1.4.36/hawtio-web-1.4.36.war') }
+  end
 end
